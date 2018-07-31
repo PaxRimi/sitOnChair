f166d244563be3aa2c92acb27fa2e8e045db1de8
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-07-31 19:21:38 +0200

    Add function that hide text from img after hover them by the mouse

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,16 @@
-console.log("Działa");
+// Add 2 img from html file
+var imgs = document.querySelectorAll('.gallery__Img');
+
+// function that remove text from img after hover on them
+
+imgs.forEach(function (element) {
+    element.addEventListener('mouseover', function (e) {
+        this.firstElementChild.classList.add('hidden');
+    })
+});
+
+imgs.forEach(function (element) {
+    element.addEventListener('mouseout', function (e) {
+        this.firstElementChild.classList.remove('hidden');
+    })
+});
